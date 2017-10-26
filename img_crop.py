8d78044f7efa0a6309dfa38c4a017d36be9dbdd3
bnhsu <b55ntw@gmail.com>
2017-10-26 16:38:25 +0800

    Update img_crop.py

--- img_crop.py
+++ img_crop.py
@@ -6,11 +6,12 @@ import numpy as np
 import glob
 
 box = (0, 142, 291, 284)
+path = '/home/username/folder/'
 for filename in glob.glob("*.png"):
 	im = Image.open(filename)
 	print im.format, im.size, im.mode
 	im_crop = im.crop(box)
 	newName = "c_" + filename
 	print newName 
-	im_crop.save(newName)
+	im_crop.save(path + newName)
 
